bf7e65869fef8aff960a962b2482a3f5c0e7dc80
GiacomoMilitello <g.militello95@gmail.com>
2024-01-23 16:20:31 +0100

    layout: from html to js

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,3 +78,43 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+console.log( posts );
+
+posts.forEach(post => {
+    const { id, content, media, author, likes } = post;
+    const { name, image } = author;
+
+    document.querySelector("#container").innerHTML += `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${image}" alt="${name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${name}</div>
+                    <div class="post-meta__time">4 mesi fa</div>
+             </div>                    
+            </div>
+        </div>
+        <div class="post__text">${content}</div>
+        <div class="post__image">
+            <img src="${media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                Piace a <b id="like-counter-${id}" class="js-likes-counter">${likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+    `
+})
